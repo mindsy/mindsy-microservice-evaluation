ef82c4cf1bac70a4185e810200ea8e1487fe900e
MatheusFilipe95 <mafieu666@hotmail.com>
2019-05-31 12:10:57 -0300

    Correção do nome da variavel classification_score

--- resources/edit_result.py
+++ resources/edit_result.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from static.imports import *
 
+
 class EditResult(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('gross_score',
@@ -26,8 +27,8 @@ class EditResult(Resource):
             if data['considerate_score']:
                 result.considerate_score = data['considerate_score']
 
-            if data['classification_score']:
-                result.classification_score = data['classification_score']
+            if data['classification']:
+                result.classification_score = data['classification']
 
         else:
             return {'message': 'Result not found.'}, 404
