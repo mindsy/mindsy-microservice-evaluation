70585a8bbd9816877d01f1c68ae39cfdbac6c72d
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-06-01 12:18:07 -0300

    Ajustando metodo para mostrar informações
    Co-authored-by: pedroMiranda7410 <pmiranda7410@gmail.com>

--- resources/show_evaluation.py
+++ resources/show_evaluation.py
@@ -6,10 +6,22 @@ from static.imports import *
 
 class ShowEvaluationID(Resource):
     def get(self, id_evaluation):
-
         evaluation = EvaluationModel.find_by_id(id_evaluation)
         if evaluation:
             evaluation_info = evaluation.json()
+            all_evaluations = evaluation.evaluation_tests.all()
+            flag = []
+            for ev in all_evaluations:
+                test_info = ev.TEST.json()
+                flag.append(test_info)
+            evaluation_info.update({'tests': flag})
+
+            flag2 = []
+            for ev in all_evaluations:
+                result_info = ev.results.json()
+                flag2.append(result_info)
+
+            evaluation_info.update({'results': flag2})
 
             return {'evaluation': evaluation_info}
 
